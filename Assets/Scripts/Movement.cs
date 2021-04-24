2499d5fcca88b77aff84e85a1f8bf34cb21ce399
BuildTools <unconfigured@null.spigotmc.org>
2021-04-24 17:37:28 -0400

    Movement is now on a grid

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private bool isMoving;
+    private Vector3 originPos, targetPos;
+    private float timetoMove = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKey(KeyCode.W) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.up));
+
+        if (Input.GetKey(KeyCode.A) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.left));
+
+        if (Input.GetKey(KeyCode.S) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.down));
+
+        if (Input.GetKey(KeyCode.D) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.right));  
+    }
+
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+        float elapsedTime=0;
+        originPos = transform.position;
+        targetPos = originPos + direction;
+
+        while(elapsedTime < timetoMove)
+        {
+            transform.position = Vector3.Lerp(originPos, targetPos, (elapsedTime / timetoMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
