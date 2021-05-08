9e209631f3b9b9edcf40654a8c151cb51c58c415
LatifaTam <59390228+LatifaTam@users.noreply.github.com>
2021-05-08 09:45:51 -0400

    UI Updated

--- Assets/Scripts/Menu_UI.cs
+++ Assets/Scripts/Menu_UI.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuInGame : MonoBehaviour
+// UI for scene 0: Menu
+public class Menu_UI : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
-   public void OnClickMenu()
-   {
-        SceneManager.LoadScene(0);
-   }
-        
-    
+    public void OnClickNewGame()
+    {
+        // check File-- buildSettings for the scene number
+        // scene 1 is the initial scene for the game
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnClickQuit()
+    {
+        //exit the program at any point
+        Application.Qiut();
+    }
 }
