30d02fdfb009ecb69d22a82dedddbe8cedb9111c
BuildTools <unconfigured@null.spigotmc.org>
2021-05-08 20:49:06 -0400

    Added rotation for movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private bool isMoving;
+    private bool isRotating;
     private Vector3 originPos, targetPos;
     private float timetoMove = 0.2f;
 
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKey(KeyCode.W) && !isMoving)
+        if (Input.GetKey(KeyCode.W) && !isMoving)
             StartCoroutine(MovePlayer(Vector3.up));
 
         if (Input.GetKey(KeyCode.A) && !isMoving)
@@ -21,7 +22,13 @@ public class Movement : MonoBehaviour
             StartCoroutine(MovePlayer(Vector3.down));
 
         if (Input.GetKey(KeyCode.D) && !isMoving)
-            StartCoroutine(MovePlayer(Vector3.right));  
+            StartCoroutine(MovePlayer(Vector3.right));
+        
+        if (Input.GetKey(KeyCode.Q) && !isMoving && !isRotating)
+            StartCoroutine(RotatePlayer(0));
+
+        if (Input.GetKey(KeyCode.E) && !isMoving && !isRotating)
+            StartCoroutine(RotatePlayer(1));
     }
 
     private IEnumerator MovePlayer(Vector3 direction)
@@ -41,5 +48,20 @@ public class Movement : MonoBehaviour
         transform.position = targetPos;
 
         isMoving = false;
+    }
+
+    private IEnumerator RotatePlayer(int d)
+    {
+        isRotating = true;
+
+        if (d == 0){
+            transform.Rotate(0f, 0f, 90f);
+        } else {
+            transform.Rotate(0f, 0f, -90f);
+        }
+
+        yield return new WaitForSeconds(0.25f);
+
+        isRotating = false;
     }
 }
